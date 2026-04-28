ccf3b49a8f721b4b666475c11c113783126751ae
Lika <donskaya.lika07@mail.ru>
2026-04-28 22:55:17 +0300

    ..

--- src/lab04/models.py
+++ src/lab04/models.py
@@ -4,8 +4,8 @@ from interfaces import Printable, Comparable
 class BankAccount:
     total_accounts = 0  
 
-    def __init__(self, account_number, owner_name, balance=0.0, account_type="Обычный"):
-        # Присваиваем напрямую без лишних функций-валидаторов
+    def __init__(self, account_number, owner_name, balance=0.0, account_type="Обычный"): 
+        
         self._account_number = account_number
         self._owner_name = owner_name
         self._balance = balance
@@ -13,7 +13,7 @@ class BankAccount:
         self._is_active = True 
         self._transactions = []
         
-        # Оставляем только базовую логику
+       
         date_str = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
         self._transactions.append(f"[{date_str}] Открытие счета: {self._balance:.2f}")
         BankAccount.total_accounts += 1
@@ -30,7 +30,7 @@ class BankAccount:
     def __str__(self):
         return f"[{self._account_type}] {self._owner_name} | Баланс: {self._balance:.2f}"
 
-# --- Классы наследники (ЛР-4) ---
+
 
 class SavingsAccount(BankAccount, Printable, Comparable):
     def __init__(self, number, owner, balance, interest_rate):
@@ -41,7 +41,6 @@ class SavingsAccount(BankAccount, Printable, Comparable):
         return f"[Savings] №{self.account_number} | Владелец: {self.owner_name} | Ставка: {self.__interest_rate}%"
 
     def compare_to(self, other) -> float:
-        # Просто сравниваем балансы
         return self.balance - other.balance
 
 class BusinessAccount(BankAccount, Printable, Comparable):
